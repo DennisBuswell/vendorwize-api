b527e0bfb9e51b4b62d7e2da00e1f80730f3b7a6
Dennis Buswell <dennisbuswell@gmail.com>
2025-12-28 16:27:55 -0500

    Use public database URL

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,11 +2,21 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema.js';
 
-const connectionString = process.env.DATABASE_URL;
+// Prefer public URL since internal networking may not be set up
+let connectionString = process.env.DATABASE_PUBLIC_URL || process.env.DATABASE_URL;
 
 if (!connectionString) {
   throw new Error('DATABASE_URL environment variable is not set');
 }
 
-const client = postgres(connectionString);
+// Clean up any trailing }} from Railway template issues
+connectionString = connectionString.replace(/\}\}$/, '');
+
+console.log('Connecting to database...');
+
+const client = postgres(connectionString, {
+  connect_timeout: 10,
+  idle_timeout: 20,
+  max_lifetime: 60 * 30,
+});
 export const db = drizzle(client, { schema });
